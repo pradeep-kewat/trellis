3f1497155847d591fa456917b84de36b5d92b5b6
pradeep-kewat <pradeep.kewat@siya.tech>
2024-10-21 16:30:22 +0530

    Delete src/utils/supabase/client.ts

--- src/utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.PRIVATE_SUPABASE_URL!,
-    process.env.PRIVATE_SUPABASE_ANON_KEY!
-  );
-}
